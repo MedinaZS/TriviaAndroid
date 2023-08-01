a65760c245d01bc8721bf17a9a43102d25912c51
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-08-01 10:10:54 -0400

    Añadir navigation bar (error)

--- app/src/main/java/com/roshka/triviaandroid/MainActivity.kt
+++ app/src/main/java/com/roshka/triviaandroid/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.roshka.triviaandroid
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
@@ -12,10 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        val navController = this.findNavController(R.id.startTriviaFragment)
-//        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
 
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = this.findNavController(R.id.startTriviaFragment)
+//        return navController.navigateUp()
+//    }
 
 }
