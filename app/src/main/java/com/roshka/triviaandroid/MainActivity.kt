535a19f7392cf831324771f70843e5bc8eaa8433
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-08-02 16:12:08 -0400

    Fix: Añadi action bar con nav controller a los fragments

--- app/src/main/java/com/roshka/triviaandroid/MainActivity.kt
+++ app/src/main/java/com/roshka/triviaandroid/MainActivity.kt
@@ -2,23 +2,32 @@ package com.roshka.triviaandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val navController = this.findNavController(R.id.nav_host_fragment)
+//        navController = this.findNavController(R.id.nav_host_fragment)
 //        NavigationUI.setupActionBarWithNavController(this, navController)
 
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
     }
 
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        val navController = this.findNavController(R.id.startTriviaFragment)
-//        return navController.navigateUp()
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
+    }
 
 }
